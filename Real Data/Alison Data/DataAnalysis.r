38e636563c7a095009a912b5c3276150526cd757
Xin Wang <xwang22@ncsu.edu>
2011-09-26 17:29:42 -0400

    fixed S matrix, delete the fake H and fake R algorithm

--- Real Data/Alison Data/DataAnalysis.r
+++ Real Data/Alison Data/DataAnalysis.r
@@ -2,6 +2,7 @@
 
 library(stats)
 library(MASS)
+library(CompQuadForm)
 rm(list= ls())
 set.seed(4)
 
@@ -73,6 +74,9 @@ TypicalIBS = function (geno){
         else S[i,j] = S[i,j] + 1/M
         p1 = p2+1
         p2 = p1+1
+      }
+      if (i==j){
+        S[i,j]=1
       }
     }
       
@@ -194,81 +198,7 @@ MaxIBS		 = function (geno){
 
 EpiTest = function(Y,X,geno1,geno2){
 
-	N 			= nrow(Y)															# the number of individual
 
-	Simi1	= TypicalIBS(geno1)
-	S1		= Simi1$S
-	H1		= Simi1$H 
-	R1		= Simi1$R
-	q1		= Simi1$L
-
-	Simi2	= TypicalIBS(geno2)
-	S2		= Simi2$S
-	H2		= Simi2$H 
-	R2		= Simi2$R
-	q2		= Simi2$L
-
-	S12 	= S1 * S2														# The similarity matrix for the interaction
-
-	p 		= ncol(X) 														# The number of fix effect
-
-	P_X 	= X %*% solve(crossprod(X,X))%*% t(X) 				# The projection of X
-
-	#----------------------- Initialize Tau and sigma ----------------------------
-
-	Tau_A_new = 1                        
-	Tau_B_new = 1
-	sigma_new = 1
-#   S2 = H2 %*% R2 %*% t(H2)
-	#------------------------------- updata --------------------------------------
-
-	repeat{		
-		Tau_A		  = Tau_A_new
-		Tau_B 	  = Tau_B_new
-		sigma 	  = sigma_new
-		
-		R 			  = rbind(cbind(Tau_A*R1,array(0,dim = c(q1,q2))),cbind(array(0,dim = c(q2,q1)),Tau_B*R2))
-		H 			  = cbind(H1,H2)
-		V0			  = H %*% R %*% t(H) + sigma * diag(N)
-
-# 		inv_V0	  = 1/sigma*(diag(N)-1/sigma*H %*% R %*% solve(diag(q1+q2)+1/sigma*t(H)%*%H%*%R)%*%t(H))
-		inv_V0    = inv(V0)
-    inv_V0X 	= inv_V0%*%X                   #    To speed up the computation
-		P 		  	= inv_V0-inv_V0X %*% solve(t(X)%*%inv_V0X)%*%t(inv_V0X)
-
-    Tau_A_new = as.numeric(1/q1 * (Tau_A^2 * t(Y) %*% P %*% S1 %*% P %*% Y + Tau_A*q1 - sum(diag( Tau_A^2 * P %*% S1))))
-    Tau_B_new = as.numeric(1/q2 * (Tau_B^2 * t(Y) %*% P %*% S2 %*% P %*% Y + Tau_B*q2 - sum(diag( Tau_B^2 * P %*% S2))))
-    
-    Y_star    = Y - Tau_A_new * S1 %*% P %*% Y - Tau_B_new * S2 %*% P %*% Y
-    
-    IP_X      = diag(N)-P_X
-    VA        =Tau_A*S1+Tau_B*S2-(Tau_A*S1+Tau_B*S2) %*% P %*% (Tau_A*S1+Tau_B*S2)
-    sigma_new = as.numeric(1/(N-p) * (t(Y_star) %*% IP_X %*% Y_star + sum(diag(IP_X%*%VA))))
-    
-    cat("new ",Tau_A_new,Tau_B_new,sigma_new,"\n")
-    
-		diff_A = abs((Tau_A_new-Tau_A)/Tau_A_new)		# use the relative difference rather than the absoult difference
-		diff_B = abs((Tau_B_new-Tau_B)/Tau_B_new)
-		diff_s = abs((sigma_new-sigma)/sigma_new)
-		if ((diff_A<0.0001) & (diff_B<0.0001) & (diff_s<0.0001)) break
-	}
-
-	P0 					  = P 
-
-	T0 					  = 1/2*t(Y) %*% P0 %*% S12 %*% P0 %*% Y	#Get T0 under null hypothesis
-	e						  = eigen(V0, symmetric=TRUE)
-	V_eigen				= e$vectors
-	V_square_root	= V_eigen %*% diag(sqrt(e$values)) %*% t(V_eigen)
-
-
-	Weights_all		= eigen(1/2*V_square_root %*% P0 %*% S12 %*% P0 %*% V_square_root, symmetric=TRUE, only.value=TRUE)
-	temp					= Weights_all$values
-	temp2					= sort(temp,decreasing=TRUE)
-	dim(temp2)		= c(N,1)
-	big_enough 		= sum(temp>10^-3)			# Get the number of big eigen values. here, the threshold for "big" is 10^-3 
-	Weights				= array(temp2[1:big_enough,1],dim=c(big_enough,1))
-
-  p = liu(T0, Weights, h = rep(1, length(Weights)), delta = rep(0, length(Weights)))
 	return(P=p)
 	
 }
@@ -388,10 +318,95 @@ geno1     = as.matrix(read.table("genoA.data",header=FALSE))
 geno2     = as.matrix(read.table("genoB.data",header=FALSE))
 # Jointp    = JointTest(Y,X,geno1,geno2)
 # Epip      = EpiTest(Y,X,geno1,geno2)
-# Gene1p    = OneGeneTest(Y,X,geno2,geno1)
-Gene2p    = OneGeneTest(Y,X,geno1,geno2)
 
-cat("The Joint test for genoA and genoB is ",Jointp,"\n")
-cat("The Epi test for genoA and genoB is ",Epip,"\n")
-cat("The One gene test for gene1 is ", Gene1p, "\n")
-cat("The One gene test for gene2 is ", Gene2p, "\n")  
+
+  N 			= nrow(Y)															# the number of individual
+
+	Simi1	= TypicalIBS(geno1)
+	S1		= Simi1$S
+	H1		= Simi1$H 
+	R1		= Simi1$R
+	q1		= Simi1$L
+
+	Simi2	= TypicalIBS(geno2)
+	S2		= Simi2$S
+	H2		= Simi2$H 
+	R2		= Simi2$R
+	q2		= Simi2$L
+
+	S12 	= S1 * S2														# The similarity matrix for the interaction
+
+	p 		= ncol(X) 														# The number of fix effect
+
+	P_X 	= X %*% solve(crossprod(X,X))%*% t(X) 				# The projection of X
+
+	#----------------------- Initialize Tau and sigma ----------------------------
+
+	Tau_A_new = 1                        
+	Tau_B_new = 1
+	sigma_new = 1
+#   S2 = H2 %*% R2 %*% t(H2)
+	#------------------------------- updata --------------------------------------
+
+	repeat{		
+		Tau_A		  = Tau_A_new
+		Tau_B 	  = Tau_B_new
+		sigma 	  = sigma_new
+		
+# 		R 			  = rbind(cbind(Tau_A*R1,array(0,dim = c(q1,q2))),cbind(array(0,dim = c(q2,q1)),Tau_B*R2))
+# 		H 			  = cbind(H1,H2)
+# 		V0			  = H %*% R %*% t(H) + sigma * diag(N)
+    
+
+# 		inv_V0	  = 1/sigma*(diag(N)-1/sigma*H %*% R %*% solve(diag(q1+q2)+1/sigma*t(H)%*%H%*%R)%*%t(H))
+		V0        = Tau_A*S1 + Tau_B*S2 + sigma*diag(N)
+    inv_V0    = solve(V0)
+    inv_V0X 	= inv_V0%*%X                   #    To speed up the computation
+		P 		  	= inv_V0-inv_V0X %*% solve(t(X)%*%inv_V0X)%*%t(inv_V0X)
+
+    Tau_A_new = as.numeric(1/q1 * (Tau_A^2 * t(Y) %*% P %*% S1 %*% P %*% Y +  Tau_A*q1 - sum(diag(Tau_A^2 * P %*% S1))))
+    Tau_B_new = as.numeric(1/q2 * (Tau_B^2 * t(Y) %*% P %*% S2 %*% P %*% Y +  Tau_B*q2 - sum(diag(Tau_B^2 * P %*% S2))))
+    
+    Y_star    = Y - Tau_A_new * S1 %*% P %*% Y - Tau_B_new * S2 %*% P %*% Y
+    
+    IP_X      = diag(N)-P_X
+    VA        =Tau_A*S1+Tau_B*S2-(Tau_A*S1+Tau_B*S2) %*% P %*% (Tau_A*S1+Tau_B*S2)
+    sigma_new = as.numeric(1/(N-p) * (t(Y_star) %*% IP_X %*% Y_star + sum(diag(IP_X%*%VA))))
+    
+    cat("new ",Tau_A_new,Tau_B_new,sigma_new,"\n")
+    
+		diff_A = abs((Tau_A_new-Tau_A)/1)		# use the relative difference rather than the absoult difference
+		diff_B = abs((Tau_B_new-Tau_B)/1)
+		diff_s = abs((sigma_new-sigma)/1)
+		if ((diff_A<0.00001) & (diff_B<0.00001) & (diff_s<0.00001)) break
+	}
+
+  P0 					  = P 
+	T0 					  = 1/2*t(Y) %*% P0 %*% S12 %*% P0 %*% Y	#Get T0 under null hypothesis
+
+  e						  = eigen(V0, symmetric=TRUE)
+	V_eigen				= e$vectors
+	V_square_root	= V_eigen %*% diag(sqrt(e$values)) %*% t(V_eigen)
+
+
+	Weights_all		= eigen(1/2*V_square_root %*% P0 %*% S12 %*% P0 %*% V_square_root, symmetric=TRUE, only.value=TRUE)
+	temp					= Weights_all$values
+	temp2					= sort(temp,decreasing=TRUE)
+	dim(temp2)		= c(N,1)
+	big_enough 		= sum(temp>10^-3)			# Get the number of big eigen values. here, the threshold for "big" is 10^-3 
+	Weights				= array(temp2[1:big_enough,1],dim=c(big_enough,1))
+
+  p = liu(T0, Weights, h = rep(1, length(Weights)), delta = rep(0, length(Weights)))
+
+
+
+
+
+
+# Gene1p    = OneGeneTest(Y,X,geno2,geno1)
+# Gene2p    = OneGeneTest(Y,X,geno1,geno2)
+
+# cat("The Joint test for genoA and genoB is ",Jointp,"\n")
+# cat("The Epi test for genoA and genoB is ",Epip,"\n")
+# cat("The One gene test for gene1 is ", Gene1p, "\n")
+# cat("The One gene test for gene2 is ", Gene2p, "\n")  
